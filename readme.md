ff50635846a6063a5970a1ce31ea3935f92461ca
xiaoyou66 <xiaoyou2333@foxmail.com>
2022-04-10 11:31:39 +0800

    prefect readme

--- readme.md
+++ readme.md
@@ -2,12 +2,68 @@
 
 ## 数据集准备
 
+数据集地址：https://github.com/luxiangju-PersonAI/iCartoonFace
+
+数据集包括两个部分检测部分和识别部分
+![](.readme_images/094a165e.png)
+
+#### 检测的数据集准备
+我们先看一下检测的数据集
+
+![](.readme_images/ef963336.png)
+
+测试的数据在那个识别的数据集里面
+
+![](.readme_images/9f909c0e.png)
+
+测试集标签是下面这个
+
+![](.readme_images/35bd1fab.png)
+
+我们对这些数据进行整理，整理出的格式如下，比如我们训练的图片放在`train/images`里去，其他的一样。
+```
+├── test
+│   ├── images
+│   └── labels
+├── train
+│   ├── images
+│   └── labels
+├── val
+│   ├── images
+│   └── labels
+├── test.txt
+├── train.txt
+└── val.txt
+```
+然后`train.txt`等几个txt就是所有图片的坐标信息了，我们先全部转换下面这样的格式，图片和坐标之间使用逗号隔开
+
+![](.readme_images/fb3fb0b0.png)
+
+当然，这种的格式不能被YOLOV5识别，我们可以参考 https://github.com/ultralytics/yolov5/wiki/Train-Custom-Data 来设置自己的数据集
+
+这里我写了一个脚本可以把我们这种格式的数据转换为yolov5的格式。转换前自己要确保我们这个检测的数据集在 `data/detection` 下。具体可以自己看看脚本内容
+```bash
+cd tool
+python set_label.py
+```
+
+转换后，我们就可以看到labels目录下有很多文本文件，一个图片对应一个文本文件，具体如下
+
+![](.readme_images/77d41ec7.png)
+
+里面的文本文件格式如下
+
+![](.readme_images/ad38db17.png)
+
+
 ## 模型训练
 
 ### yolov5 模型训练
 ```bash
 # 首先我们克隆一下yolov5项目
+git clone https://github.com/ultralytics/yolov5.git
 cd yolov5
-python train.py --img 640 --batch 16 --epochs 50 --data dataset.yaml --weights yolov5m.pt
+# 需要注意，我们需要修改models下yolov5m.yaml的nc=1，表示我们只有一个类别。如果你选择的其他模型，也是一样的
+# 开始训练我们的数据集
+python train.py --img 640 --batch 16 --epochs 50 --data ../dataset/dataset.yaml --weights yolov5m.pt
 ```
-
--- tool/set_label.py
+++ tool/set_label.py
@@ -2,8 +2,9 @@
 from cv2 import cv2
 import os
 
-### 这一块是显示yolo的标签
-def show_yolo_img(filename,point):
+
+# 这一块是显示yolo的标签
+def show_yolo_img(filename, point):
     point = str(point).split(" ")
     # 对应的文件路径
     img = cv2.imread(filename)
@@ -12,13 +13,14 @@ def show_yolo_img(filename,point):
     width = img.shape[1]
     print(height, width)
     # 计算左上和右下角的坐标
-    x1 = int((float(point[1]) - float(point[3])/2) * width)
-    y1 = int((float(point[2]) - float(point[4])/2) * height)
-    x2 = int((float(point[1]) + float(point[3])/2) * width)
-    y2 = int((float(point[2]) + float(point[4])/2) * height)
+    x1 = int((float(point[1]) - float(point[3]) / 2) * width)
+    y1 = int((float(point[2]) - float(point[4]) / 2) * height)
+    x2 = int((float(point[1]) + float(point[3]) / 2) * width)
+    y2 = int((float(point[2]) + float(point[4]) / 2) * height)
     cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
     cv2.imshow("a", img)
     cv2.waitKey(-1)
+
 
 # 显示标注图片的链接
 def show_label():
@@ -27,7 +29,7 @@ def show_label():
     files = os.listdir(path)
     # 读取文件
     for file in files:
-        with open(path+file) as f:
+        with open(path + file) as f:
             filename = path + "../images/" + file[:-4] + ".jpg"
             for point in f.readlines():
                 show_yolo_img(filename, point)
@@ -36,18 +38,20 @@ def show_label():
 
 # 所有的检测图片路径
 detection_path = "../data/detection"
-### 这一块是开始计算标签
+
+
+# 这一块是开始计算标签
 def calculate_label(data_type):
     # 首先我们读取标签
-    with open("%s/%s.txt" % (detection_path,data_type)) as f:
+    with open("%s/%s.txt" % (detection_path, data_type)) as f:
         for item in f.readlines():
             data = str(item).split(",")
             if len(data) >= 5:
                 name = data[0]
                 # 计算对应的标签名字
-                label_name = "%s/%s/labels/%s.txt" % (detection_path,data_type,name[:name.find(".")])
+                label_name = "%s/%s/labels/%s.txt" % (detection_path, data_type, name[:name.find(".")])
                 # 先读取图片获取图片的坐标
-                filename = "%s/%s/images/%s" % (detection_path,data_type,name)
+                filename = "%s/%s/images/%s" % (detection_path, data_type, name)
                 print(filename)
                 shape = cv2.imread(filename).shape
                 height = shape[0]
@@ -55,17 +59,20 @@ def calculate_label(data_type):
                 # 计算中心点
                 point = [
                     "0",
-                    str((float(data[3]) + float(data[1]))/2 / float(width)),
-                    str((float(data[4]) + float(data[2]))/2 / float(height)),
-                    str((float(data[3]) - float(data[1]))/float(width)),
-                    str((float(data[4]) - float(data[2]))/float(height))
+                    str((float(data[3]) + float(data[1])) / 2 / float(width)),
+                    str((float(data[4]) + float(data[2])) / 2 / float(height)),
+                    str((float(data[3]) - float(data[1])) / float(width)),
+                    str((float(data[4]) - float(data[2])) / float(height))
                 ]
                 # 写入文件
-                with open(label_name,"a") as f2:
-                    f2.write(" ".join(point)+"\n")
+                with open(label_name, "a") as f2:
+                    f2.write(" ".join(point) + "\n")
         print("遍历完成")
+
 
 if __name__ == '__main__':
     # 开始计算各项指标
-    # calculate_label("val")
-    show_label()
+    calculate_label("train")
+    calculate_label("test")
+    calculate_label("val")
+    # show_label()
